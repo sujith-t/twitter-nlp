d9f020c09444888c1e9b6d64cbffc7c024152dca
Sujith T <exodus123@gmail.com>
2022-06-29 22:20:26 +0530

    completed get news items

--- script.py
+++ script.py
@@ -8,18 +8,28 @@ Created on Tue Jun 28 21:18:25 2022
 import snscrape.modules.twitter as sntwitter
 from datetime import date, datetime
 import pandas as pd
+import re
+
+web_url_regex = "((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)"
 
 # dynamically get sports related 10,000 tweets from last year
-today = date.today()
-from_date = datetime(today.year - 1, today.month, today.day)
-query = "(sports) lang:en until:%s since:%s" % (today.strftime("%Y-%m-%d"), from_date.strftime("%Y-%m-%d"))
 limit = 5
 tweets = []
+today = date.today()
+from_date = datetime(today.year - 1, today.month, today.day)
+query = "\"sports news\" lang:en until:%s since:%s" % (today.strftime("%Y-%m-%d"), from_date.strftime("%Y-%m-%d"))
 
 for tweet in sntwitter.TwitterSearchScraper(query).get_items():
     if len(tweets) == limit:
         break
-    tweet.
-    tweets.append([tweet.date, tweet.user.username, tweet.content])
     
-dt = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+    urls = re.findall(web_url_regex, tweet.content)
+    news_url = ''
+    for x in urls:
+        news_url = x[0].strip()
+        
+    #print(vars(tweet))
+    if len(news_url) > 0:
+        tweets.append([tweet.date, tweet.user.username, tweet.content, news_url])
+    
+dt = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet', 'NewsFeed_URL'])
